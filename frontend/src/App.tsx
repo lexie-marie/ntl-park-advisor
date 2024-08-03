0ec7fdfe1fabb0396fb24f2e8228fe416b06c099
Lexie Marinelli <lexie.marinelli@gmail.com>
2024-08-03 14:45:20 -0600

    add more tailwind styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen flex flex-col min-w-full">
+          <header className="text-gray-700 text-center p-4 font-bold">
+              National Park Advisor
+          </header>
+          <div className="p-52"/>
+          <button className="bg-stone-200 text-forest-green outline-sage-green text-center p-4 hover:outline-sage-green">Get Your Recommendations</button>
+          <main className="flex-grow container mx-auto p-4 flex-col">
+          </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+)
 }
 
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'silver': '#ecebff',
+        'brown': '#AA6A2B',
+        'gold':'#c5b867',
+        'light-green':'#9bdd7a',
+        'med-green':'#329c64',
+        'dark-green':'#3c6738',
+        'darker-green': '#1D4233',
+        'forest-green': '#569151',
+      },
+    },
   },
   plugins: [],
 }
