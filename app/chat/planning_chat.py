1703ee93530716f13c985a917d6b7a40760ddb09
Sarah Kainec <Sarah.Kainec@focusedlabs.io>
2024-08-04 08:22:51 -0600

    adding the chat for planning

---
+++ app/chat/planning_chat.py
@@ -0,0 +1,69 @@
+from operator import itemgetter
+
+from langchain_core.documents import Document
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel
+
+from app.llms.llms import groq_chat
+
+prompt_template = """
+# Context
+=================
+You are an expert at planning National Park trips. 
+You are knowledgeable about all the things to consider when planning a trip to a National Park
+
+# Instructions
+=================
+Create an itinerary for a National Park trip to {destination} during the {season}.
+The trip will be {duration} long and the traveler is interested in {interests}.
+Give information about the weather so the traveler can pack accordingly.
+Additionally, create a markdown table with the following columns:
+- Day
+- Activity
+- Time
+- Location
+Be sure to create a detailed itinerary that includes all the activities and locations the traveler should visit each day.
+        
+# Start Analysis
+=================
+Given the following context:
+{context}
+
+Create an itinerary:
+"""
+
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["destination", "season", "duration", "interests", "context"])
+
+
+# def transform_transcripts(state: dict) -> list[Document]:
+#     print("transforming transcripts")
+#     transcript_documents = []
+#     for video in state["transcript_data"]:
+#         transcript_documents.append(Document(video['transcript']))
+#     return transcript_documents
+
+
+def chat_with_planning_docs(state: dict) -> dict:
+    print("chatting with docs")
+
+    # transcripts = transform_transcripts(state)
+
+    chain = (RunnableParallel(
+        context=itemgetter("context"),
+        destination=itemgetter("destination"),
+        season=itemgetter("season"),
+        duration=itemgetter("duration"),
+        interests=itemgetter("interests")
+    ) | PROMPT | groq_chat)
+    answer = chain.invoke(
+        {"destination": state["destination"],
+         "season": state["season"],
+         "duration": state["duration"],
+         "interests": state["interests"],
+         "context": state["relevant_docs"] # + transcripts,
+         })
+    print(answer)
+    return {
+        **state,
+        "answer": answer.content
+    }
--- app/graph/building_planning_graph.py
+++ app/graph/building_planning_graph.py
@@ -1,6 +1,7 @@
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 
+from app.chat.planning_chat import chat_with_planning_docs
 from app.graph.graph_planning_state import PlanningGraphState
 from app.retrievers.doc_store.planning_get_docs_from_vectorstore import get_planning_docs_from_vector_store
 from app.validate_docs.validate_planning_docs import validate_planning_docs
@@ -10,12 +11,12 @@ workflow = StateGraph(PlanningGraphState)
 workflow.add_node("get_docs_from_vector_store", get_planning_docs_from_vector_store)
 workflow.add_node("validate_planning_docs", validate_planning_docs)
 # workflow.add_node("get_videos", get_videos_from_twelve)
-# workflow.add_node("chat_with_docs", chat_with_docs)
+workflow.add_node("chat_with_docs", chat_with_planning_docs)
 
 workflow.add_edge("get_docs_from_vector_store", "validate_planning_docs")
-workflow.add_edge("validate_planning_docs", END)
+workflow.add_edge("validate_planning_docs", "chat_with_docs")
 # workflow.add_edge("get_videos", "chat_with_docs")
-# workflow.add_edge("chat_with_docs", END)
+workflow.add_edge("chat_with_docs", END)
 
 workflow.set_entry_point("get_docs_from_vector_store")
 planning_graph = workflow.compile()
