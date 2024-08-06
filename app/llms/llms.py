2c89c89f32022c7414448cf21c43fb9c15c61610
Sarah Kainec <Sarah.Kainec@focusedlabs.io>
2024-08-06 10:43:47 -0600

    updating model

--- app/llms/llms.py
+++ app/llms/llms.py
@@ -2,4 +2,4 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 
 load_dotenv()
-groq_chat = ChatGroq(temperature=0, model_name="llama-3.1-8b-instant")
+groq_chat = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile")
